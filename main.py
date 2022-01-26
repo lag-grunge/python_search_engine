7e3b430c151f1f045b4903187b584885df9ab7de
sdalton <sdalton@student.21-school.ru>
2022-01-26 18:43:51 +0300

    default page, docs.json small fix

--- main.py
+++ main.py
@@ -15,17 +15,19 @@ parser.add_argument('query')
 parser.add_argument('delete', type=int)
 
 
-@app.route('/?query', methods=['GET'])
+@app.route('/', methods=['GET'])
 def query_pages():
     args = parser.parse_args()
     data = args['query']
+    if not data:
+        return default_help()
     res = search(data, index, pages, ranking=False)
     if len(res) == 0:
         return jsonify({'error': 'data not found'})
     elif len(res) > 0:
         res_t = {str(res[p]):str(pages[res[p]]) for p in range(min(sizequery, len(res)))}
         res_t['score'] = "{0} results, {1} output, query stops".format(len(res), len(res_t))
-        return jsonify(res_t)
+        return res_t
 
 
 @app.route('/', methods=['DELETE'])
@@ -42,7 +44,6 @@ def delete_page():
     res_t['result'] = "id {0} page deleted".format(data)
     return jsonify(res_t)
 
-@app.route('/')
 def default_help():
     with open('docs.json', 'r') as f:
         return f.read()
